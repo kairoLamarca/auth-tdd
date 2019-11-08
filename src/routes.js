87c947b9dff4542884d7454f15d3cc013d8f9b5d
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-08 19:39:06 -0300

    configuração do index e criação de um usuário

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
 
-// Definição de rotas
+User.create({name: "Kairo", email: "kairo@gmail.com", password_hash: "6468468468486468"});
 
 module.exports = routes;
