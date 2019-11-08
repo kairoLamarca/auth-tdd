101d2198e7aec9cd5bba1b5c93357d9adc19db97
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-08 19:42:07 -0300

    configuração do nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,3 @@
 const routes = require('express').Router();
-const { User } = require('./app/models');
-
-User.create({name: "Kairo", email: "kairo@gmail.com", password_hash: "6468468468486468"});
 
 module.exports = routes;
