cd629f67afcb3ef80b71a8877980c057282a183d
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-08 20:05:25 -0300

    criação dos .env e configuração do dotenv

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,14 @@
+describe('Authentication', () => {
+    it('should sum two numbers', () => {
+        const x = 2;
+        const y = 4;
+
+        const sum = x + y;
+
+        expect(sum).toBe(7);
+    })
+
+    // it('should receive JWT token when authenticated with valid credentials', () => {
+
+    // })
+});
