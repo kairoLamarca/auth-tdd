2dc34e4cf463d93ea8fac0210d27f45dd52e74d3
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-09 13:01:35 -0300

    Code da rota para nao dar erro e criação de truncate das tabelas antes dos testes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,7 @@
 const routes = require('express').Router();
 
+const SesssionController = require('./app/controllers/SessionController');
+
+routes.post('/sessions', SesssionController.store);
+
 module.exports = routes;
