26e56b67126b05e0dee911d38f24e37753c9f5f0
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-09 12:43:57 -0300

    instalção do supertest eprimeiro teste com erro

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,15 +1,20 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
 const { User } = require('../../src/app/models');
 
 describe('Authentication', () => {
-    it('should sum two numbers', async () => {
-        const user = await User.create({name: "Kairo", email: "kairo@gmail.com", password_hash: "123456"});
+    it('should authenticate with valid credentials', async () => {
+        const user = await User.create({ name: "Kairo", email: "kairo@gmail.com", password_hash: "123123" });
 
-        console.log(user);
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123456'
+            });
 
-        expect(user.email).toBe('kairo@gmail.com');
+        expect(response.status).toBe(200);
     })
-
-    // it('should receive JWT token when authenticated with valid credentials', () => {
-
-    // })
 });
